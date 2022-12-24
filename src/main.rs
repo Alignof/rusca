ece61d971fe016fcd90725b909d97f4148d0b9a7
Alingof <Alignof@outlook.com>
2022-12-24 16:23:10 +0900

    [add] implement to read file and store data

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use std::collections::HashMap;
+use std::fs::File;
+use std::io::prelude::*;
+use std::io::BufReader;
+
 fn main() {
-    println!("Hello, world!");
+    let mut filename = home::home_dir().unwrap();
+    filename.push("tmp/ClamavDatabase/main.ndb");
+    let file = File::open(filename.as_path()).unwrap();
+    let reader = BufReader::new(file);
+    let mut signeture: HashMap<String, String> = HashMap::new();
+
+    for line in reader.lines() {
+        let properties: Vec<String> = line.unwrap().split(':').map(|x| x.to_string()).collect();
+        signeture.insert(properties[0].clone(), properties[3].clone());
+    }
+
+    for sig in signeture.iter() {
+        println!("{}, {}", sig.0, sig.1);
+    }
 }
